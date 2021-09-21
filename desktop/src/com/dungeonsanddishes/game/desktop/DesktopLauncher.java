bf0c505fb4ae9bc7d2737dfdc78d1a920048138b
Lasse Andresen <lasseandresen.1996@hotmail.com>
2021-09-21 14:47:50 +0200

    Initial commit

---
+++ desktop/src/com/dungeonsanddishes/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dungeonsanddishes.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dungeonsanddishes.game.DungeonsAndDishesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonsAndDishesGame(), config);
+	}
+}
