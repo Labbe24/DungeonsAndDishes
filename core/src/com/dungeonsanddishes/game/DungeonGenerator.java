8cb74f096285abfc123939c66ae17290a502f889
Lasse Andresen <lasseandresen.1996@hotmail.com>
2021-10-02 18:38:27 +0200

    fixed some out of bounds errors

--- core/src/com/dungeonsanddishes/game/DungeonGenerator.java
+++ core/src/com/dungeonsanddishes/game/DungeonGenerator.java
@@ -1,8 +1,10 @@
 package com.dungeonsanddishes.game;
 
-import java.lang.Math;
+import java.util.Random;
 
 public class DungeonGenerator {
+
+    private Random random = new Random();
 
     private boolean[][] create2DArray(int dimensions){
         boolean [][] arr = new boolean[dimensions][dimensions];
@@ -25,34 +27,34 @@ public class DungeonGenerator {
         boolean [][] dungeonMap = create2DArray(dimensions);
 
         // place the random walker at a random cell
-        int currentRow = (int)(Math.random() * dimensions);
-        int currentColumn = (int)(Math.random() * dimensions);
+        int currentRow = random.nextInt(dimensions-1);
+        int currentColumn = random.nextInt(dimensions-1);
 
         int [][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int [][] lastDirection = new int[1][1];
-        int [][] randomDirection = new int[1][1];
+        int [] lastDirection = {0, 0};
+        int [] randomDirection = {0, 0};
 
         while(dimensions > 0 && maxTunnels > 0 && maxLength > 0)
         {
             // set a direction for random walker
             // direction should be perpendicular to last direction
             do{
-                randomDirection[0] = directions[(int)(Math.random() * directions.length)];
-            } while((randomDirection[0][0] == -lastDirection[0][0] && randomDirection[0][1] == -lastDirection[0][1]
-                    || randomDirection[0][0] == lastDirection[0][0] && randomDirection[0][1] == lastDirection[0][1]));
+                randomDirection = directions[random.nextInt(directions.length)];
+            } while((randomDirection[0] == -lastDirection[0] && randomDirection[1] == -lastDirection[1])
+                    || (randomDirection[0] == lastDirection[0] && randomDirection[1] == lastDirection[1]));
 
             // set random length
-            int randomLength = (int)(Math.random() * maxLength);
+            int randomLength = random.nextInt(dimensions);
             int tunnelLength = 0;
 
             // let random walker walk the random length
             while(tunnelLength < randomLength)
             {
                 // stop if random walker is going beyond array
-                if((currentRow == 0 && randomDirection[0][0] == -1) ||
-                        (currentColumn == 0 && randomDirection[0][1] == -1) ||
-                        ((currentRow == 0 && randomDirection[0][0] == 1)) ||
-                        ((currentColumn == 0 && randomDirection[0][1] == 1)))
+                if((currentRow == 0 && randomDirection[0] == -1) ||
+                        (currentColumn == 0 && randomDirection[1] == -1) ||
+                        ((currentRow == dimensions - 1 && randomDirection[0] == 1)) ||
+                        ((currentColumn == dimensions - 1 && randomDirection[1] == 1)))
                 {
                     break;
                 }
@@ -60,8 +62,8 @@ public class DungeonGenerator {
                 // and prepare random walker to walk to next cell
                 else {
                     dungeonMap[currentRow][currentColumn] = true;
-                    currentRow += randomDirection[0][0];
-                    currentColumn += randomDirection[0][1];
+                    currentRow += randomDirection[0];
+                    currentColumn += randomDirection[1];
                     tunnelLength++;
                 }
             }
