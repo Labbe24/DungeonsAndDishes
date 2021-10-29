b23ca11aee8fad906f8725a0fa82c4cb62dab077
VictorSteiner <victorsteiner91@gmail.com>
2021-10-29 15:41:28 +0200

    removed console log

--- core/src/com/dungeonsanddishes/game/Seeker.java
+++ core/src/com/dungeonsanddishes/game/Seeker.java
@@ -27,36 +27,10 @@ public class Seeker {
             return;
         }
 
-        // int angle = (int) calculateAngle(c_point, e_point);
-
         _enemy.setAccelerationVec(new Vector2(c_point.x - e_point.x, c_point.y - e_point.y));
     }
 
     private static float distance(Point a, Point b) {
         return (float) Math.sqrt((b.y - a.y) * (b.y - a.y) + (b.x - a.x) * (b.x - a.x));
-    }
-
-    private static int calculateAngle(Point a, Point b)
-    {
-        double angle = Math.toDegrees(Math.atan2(a.x - b.x, a.y - b.y) + Math.PI/2);
-        System.out.println(angle);
-        if (angle < 0) {
-            angle += 360;
-        }
-
-        if (angle > 0 && angle < 90) {
-            angle += 90;
-        } else if (angle < 180 && angle > 90) {
-            angle -= 90;
-        } else if (angle > 180 && angle < 270) {
-            angle += 90;
-        } else {
-            angle -= 90;
-        }
-
-        System.out.print(" ");
-        System.out.print(angle);
-
-        return (int) angle;
     }
 }
