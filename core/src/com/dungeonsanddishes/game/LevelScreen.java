c754cc256b5a05ed4e6e24bd0814dc6ab7cd5d30
Jens Kristensen <jens-nk@hotmail.com>
2021-11-22 18:16:04 +0100

    main menu -> game -> game over | screen cycle

--- core/src/com/dungeonsanddishes/game/LevelScreen.java
+++ core/src/com/dungeonsanddishes/game/LevelScreen.java
@@ -100,6 +100,8 @@ public class LevelScreen extends BaseScreen
         else{
             //game over
             Logger.getGlobal().log(Level.WARNING,"GAME OVER!!!!");
+            this.dispose();
+            game.setScreen( new GameOverScreen(this.game));
         }
     }
             //check if interactible nearby
